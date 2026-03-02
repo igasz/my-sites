1d91d7b57f27768af499563a89031ebb4514efa9
igasz <igaszaf@gmail.com>
2026-03-02 08:41:13 +0100

    portfolio

---
+++ portfolio/index.js
@@ -0,0 +1,30 @@
+const btnAbout = document.getElementById('btn-about');
+        const btnExperience = document.getElementById('btn-experience');
+        const btnProjects = document.getElementById('btn-projects');
+
+        const secAbout = document.getElementById('about');
+        const secExperience = document.getElementById('experience');
+        const secProjects = document.getElementById('projects');
+
+
+        function hideAllSections() {
+            secAbout.classList.remove('active-section');
+            secExperience.classList.remove('active-section');
+            secProjects.classList.remove('active-section');
+        }
+
+        
+        btnAbout.addEventListener('click', () => {
+            hideAllSections(); 
+            secAbout.classList.add('active-section');
+        });
+
+        btnExperience.addEventListener('click', () => {
+            hideAllSections();
+            secExperience.classList.add('active-section');
+        });
+
+        btnProjects.addEventListener('click', () => {
+            hideAllSections();
+            secProjects.classList.add('active-section');
+        });
